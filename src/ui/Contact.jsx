a32cbb8d0d3fda778a44a9e7414bdc5c28355c8d
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-07 13:33:06 +0800

    adding features

--- src/ui/Contact.jsx
+++ src/ui/Contact.jsx
@@ -1,7 +1,48 @@
 function Contact() {
   return (
-    <div className="text-[#0a192f]">
-      <h1>Contact</h1>
+    <div
+      name="contact"
+      className="bg-[#D3D3D3] w-full h-screen flex justify-center items-center p-4"
+    >
+      <form
+        method="POST"
+        action="https://getform.io/f/66fdaf41-87cf-405e-8171-2297ecd141a2"
+        className="flex flex-col max-w-[600px] w-full"
+      >
+        <div className="pb-8 text-center">
+          <p className="text-4xl font-bold inline border-b-4 border-[#0a192f] text-#0a192f">
+            Contact me here:
+          </p>
+          <p className="text-stone-300 text-lg pt-4">
+            Submit the form below or shoot me an email -
+            marpauljaybunani1314@gmail.com
+          </p>
+        </div>
+        <input
+          className="bg-yellow-800 p-2"
+          type="text"
+          placeholder="Name"
+          name="name"
+        />
+        <input
+          className="my-4 p-2 bg-yellow-800"
+          type="text"
+          placeholder="Email"
+          name="email"
+        />
+        <textarea
+          className="bg-[#0a192f] p-2"
+          name="message"
+          rows="10"
+          placeholder="Message"
+        ></textarea>
+        <button
+          className="text-[#0a192f] border-2 border-[#0a192f] hover:bg-[#0a192f] hover:text-white
+         hover:font-bold px-4 py-3 my-8 mx-auto flex items-center"
+        >
+          Let&apos;s Collaborate
+        </button>
+      </form>
     </div>
   );
 }
--- src/ui/Skills.jsx
+++ src/ui/Skills.jsx
@@ -1,12 +1,74 @@
+import htmlImg from "../assets/html.png";
+import cssImg from "../assets/css.png";
+import jsImg from "../assets/javascript.png";
+import reactImg from "../assets/react.png";
+import tailwindImg from "../assets/tailwind.png";
+import reactRouterImg from "../assets/react-router.png";
+import githubImg from "../assets/github.png";
+import reduxImg from "../assets/redux1.png";
+
 function Skills() {
   return (
     <div name="skills" className="bg-[#D3D3D3] text-[#0a192f] w-full h-screen">
       {/*Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justtify-center w-full h-full">
-        <div className="flex justify-center">
-          <p className="text-5xl sm:text-4xl font-bold inline border-b-4 border-[#0a192f] ">
+        <div className="flex justify-center pt-15 sm:pt-20">
+          <p className="text-5xl sm:text-4xl font-bold inline border-b-4 border-[#0a192f] pt-[80px]">
             Skills
           </p>
+        </div>
+        <p className="py-4 text-2xl flex justify-center">
+          These are the technologies I&apos;ve worked with.
+        </p>
+
+        <div className="w-full grid grid-cols-2 sm:grid-cols-4 text-center gap-4 p-8">
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={htmlImg} alt="html logo" />
+            <p className="my-4 ">HTML</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={cssImg} alt="css logo" />
+            <p className="my-4 ">CSS</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={jsImg} alt="javascript logo" />
+            <p className="my-4 ">Javascript</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={reactImg} alt="react logo" />
+            <p className="my-4 ">ReactJs</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img
+              className="w-20 mx-auto"
+              src={tailwindImg}
+              alt="tailwindcss logo"
+            />
+            <p className="my-4 ">Tailwind CSS</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img
+              className="w-20 mx-auto pt-5"
+              src={reactRouterImg}
+              alt="react-router logo"
+            />
+            <p className="my-4 pt-4">React-router</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={reduxImg} alt="redux logo" />
+            <p className="my-4 ">Redux</p>
+          </div>
+
+          <div className="shadow-md shadow-yellow-800 hover:scale-110 hover:bg-[#0a192f] hover:text-white duration-500">
+            <img className="w-20 mx-auto" src={githubImg} alt="github logo" />
+            <p className="my-4 ">Github</p>
+          </div>
         </div>
       </div>
     </div>
