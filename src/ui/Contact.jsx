ca098b61737dd94de1897ebb368d9b375cd4d910
Marpaul Jay Bunani <marpauljaybunani1314@gmail.com>
2023-10-07 15:48:44 +0800

    test run1

--- src/ui/Contact.jsx
+++ src/ui/Contact.jsx
@@ -9,11 +9,11 @@ function Contact() {
         action="https://getform.io/f/66fdaf41-87cf-405e-8171-2297ecd141a2"
         className="flex flex-col max-w-[600px] w-full"
       >
-        <div className="pb-8 text-center">
-          <p className="text-4xl font-bold inline border-b-4 border-[#0a192f] text-#0a192f  hover:text-yellow-800 duration-1000 hover:cursor-default hover:border-yellow-800">
+        <div className="pb-2 sm:pb-8 text-center">
+          <p className="text-2xl sm:text-4xl font-bold inline border-b-4 border-[#0a192f] text-#0a192f  hover:text-yellow-800 duration-1000 hover:cursor-default hover:border-yellow-800">
             Contact me here:
           </p>
-          <p className="text-[#0a192f] text-xl pt-4  hover:text-yellow-800 hover:cursor-default">
+          <p className="text-[#0a192f] text-base sm:text-xl pt-2 sm:pt-4 hover:text-yellow-800 hover:cursor-default">
             Submit the form below or shoot me an email -
             <span className="hover:italic">marpauljaybunani1314@gmail.com</span>
           </p>
@@ -38,7 +38,7 @@ function Contact() {
         ></textarea>
         <button
           className="text-[#0a192f] border-2 border-[#0a192f] hover:bg-[#0a192f] hover:text-white
-         hover:font-bold px-4 py-3 my-8 mx-auto flex items-center"
+         hover:font-bold px-4 py-1 sm:py-3 my-2 sm:my-8 mx-auto flex items-center"
         >
           Let&apos;s Collaborate
         </button>
